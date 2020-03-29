db01e17e4e3d952b2f7dcdb5a811e1e1018215c1
kusa11 <59110220+kusa11@users.noreply.github.com>
2020-03-29 20:34:10 +0900

    migrate2

--- db/migrate/20200308084443_devise_create_users.rb
+++ db/migrate/20200308084443_devise_create_users.rb
@@ -7,6 +7,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :nickname, null: false, unique: true, index: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :birth_year, null: false
+      t.integer :birth_month, null: false
+      t.integer :birth_day, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name_kana, null: false
+      t.string :last_name_kana, null: false
+      t.string :phone_number, null: false
 
       ## Recoverable
       t.string   :reset_password_token
