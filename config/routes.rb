7d92b3b3da31b16bb83ce04ac2627b3ca406846b
kusa11 <59110220+kusa11@users.noreply.github.com>
2020-04-02 20:14:06 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only: [:index, :new]
+
+  resources :signup do
+    collection do
+      get 'first'
+
+    end
+  end
+
+
 end
+
+
