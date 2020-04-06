c0c8ba1accab4b22ea4b16900e8e9df00bbba918
kusa11 <59110220+kusa11@users.noreply.github.com>
2020-04-06 20:50:35 +0900

    コンフリクトの解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, only: [:index, :new]
-
-  resources :signup do
-    collection do
-      get 'first'
-      get 'second'
-      get 'third'
-
-    end
-  end
-
-
 end
 
 
