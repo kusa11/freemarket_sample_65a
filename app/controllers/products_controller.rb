f26855891243a35885e56320902b65e28d2ef376
gasashi <gasashi8264@gmail.com>
2020-04-07 10:10:17 +0900

    Revert "Revert "商品出品(サーバーサイド)""
    This reverts commit 596ef14855bacfb437daaccf650f1d407357e3e4.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,31 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    @product.images.build()
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
   end
+
+  def edit
+  end
   
+  private
+    def product_params
+        params.require(:product).permit(:name, :brand, :description, :condition, :delivery_fee, :delivery_area, :delivery_day, :price, :delivery_day, :price, :category_id, :user_id, images_attributes: [:image, :_destroy, :id])
+    end
+
+    def product_find
+        @product = Product.find(params[:id])
+    end
 end
