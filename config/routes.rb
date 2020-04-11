249743700d9dc0b50de7add82ce32822583049b4
gasashi <gasashi8264@gmail.com>
2020-04-11 17:10:07 +0900

    top-markup2

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-
   resources :signup do
     collection do
       get 'first'
       get 'second'
       get 'third'
-
-
     end
   end
+  resources :products, only: [:index, :new, :show]
 end
 
 
