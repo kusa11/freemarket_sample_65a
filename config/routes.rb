27b0e29217e84df0c46778ae27e0b92b484079fd
kusa11 <59110220+kusa11@users.noreply.github.com>
2020-05-01 01:40:13 +0900

    device controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
+    registrations: 'users/registrations'
+}
   root 'products#index'
   resources :products, only: [:index, :new, :show]
 
