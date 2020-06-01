e4873ec4d017384792a1fdde18e28cb97843dc57
kusa11 <59110220+kusa11@users.noreply.github.com>
2020-06-01 19:55:58 +0900

    validate2

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,7 @@
 class SignupController < ApplicationController
+  before_action :validates_step1, only: :third
+  before_action :validates_step2, only: :fifth
+  before_action :validates_step3, only: :create
   
   def first
   end
@@ -8,24 +11,11 @@ class SignupController < ApplicationController
   end
 
   def third
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:birth_year] = user_params[:birth_year]
-    session[:birth_month] = user_params[:birth_month]
-    session[:birth_day] = user_params[:birth_day]    
-    session[:user_first_name] = user_params[:first_name] 
-    session[:user_last_name] = user_params[:last_name]
-    session[:user_first_name_kana] = user_params[:first_name_kana]
-    session[:user_last_name_kana] = user_params[:last_name_kana]
-    
     @user = User.new
   end
 
   
   def fourth
-    session[:phone_number] = user_params[:phone_number]
-    
     @user = User.new
   end
 
@@ -34,21 +24,80 @@ class SignupController < ApplicationController
     @address = @user.build_address
   end
 
+  def validates_step1
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:user_first_name] = user_params[:first_name] 
+    session[:user_last_name] = user_params[:last_name]
+    session[:user_first_name_kana] = user_params[:first_name_kana]
+    session[:user_last_name_kana] = user_params[:last_name_kana]
+    session[:birth_year] = user_params[:birth_year]
+    session[:birth_month] = user_params[:birth_month]
+    session[:birth_day] = user_params[:birth_day]
+    
 
-  def create
     @user = User.new(
       nickname: session[:nickname],
       email: session[:email],
       password: session[:password],
-      birth_year: session[:birth_year],
-      birth_month: session[:birth_month],
-      birth_day: session[:birth_day],
+      password_confirmation: session[:password_confirmation],
       first_name: session[:user_first_name],
       last_name: session[:user_last_name],
       first_name_kana: session[:user_first_name_kana],
       last_name_kana: session[:user_last_name_kana],
+      birth_year: session[:birth_year],
+      birth_month: session[:birth_month],
+      birth_day: session[:birth_day],
+    )
+    @user.errors.full_messages
+    render second_signup_index_path unless @user.valid?(:validates_step1)
+
+  end
+
+  def validates_step2
+    session[:phone_number] = user_params[:phone_number]
+
+    @user = User.new(phone_number: session[:phone_number])
+    render third_signup_index_path unless @user.valid?(:validates_step2)
+
+  end
+
+  def validates_step3
+
+    session[:last_name] = user_params[:address_attributes][:last_name]
+    session[:first_name] = user_params[:address_attributes][:first_name]
+    session[:last_name_kana] = user_params[:address_attributes][:last_name_kana]
+    session[:first_name_kana] = user_params[:address_attributes][:first_name_kana]
+    session[:post_number] = user_params[:address_attributes][:post_number]
+    session[:prefecture_id] = user_params[:address_attributes][:prefecture_id]
+    session[:city] = user_params[:address_attributes][:city]
+    session[:address_number] = user_params[:address_attributes][:address_number]
+    session[:building] = user_params[:address_attributes][:building]
+    session[:phone_number] = user_params[:address_attributes][:phone_number]
+
+    @user = User.new
+    
+    @address = Address.new(
+      last_name: session[:last_name],
+      first_name: session[:first_name],
+      last_name_kana: session[:last_name_kana],
+      first_name_kana: session[:first_name_kana],
+      post_number: session[:post_number],
+      prefecture_id: session[:prefecture_id],
+      city: session[:city],
+      address_number: session[:address_number],
+      building: session[:building],
       phone_number: session[:phone_number]
     )
+    @user.address = @address
+    render fifth_signup_index_path unless @address.valid?(:validates_step3)
+  end
+
+
+  def create
+    
     @user.build_address(user_params[:address_attributes])
     if @user.save
       @user.address.user_id = @user.id
@@ -57,6 +106,12 @@ class SignupController < ApplicationController
     else
         render first_signup_index_path
     end
+  end
+
+  def done
+    sign_in User.find(session[:id]) unless user_signed_in?
+
+    redirect_to new_user_card_path(session[:id])
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,34 +14,33 @@ class User < ApplicationRecord
   VALID_POSTAL_CODE = /\A\d{3}-?\d{4}\z/
 
   # ユーザー情報
-  validates :nickname, presence: true, length: {maximum: 20}
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'は有効でありません。' }
-  validates :password, presence: true, length: {minimum: 7, maximum: 30}, format: { with: VALID_PASSWORD_REGEX, message: 'は255文字以下に設定して下さい'}, on: :create
-  validates :birth_year, presence: true
-  validates :birth_month, presence: true
-  validates :birth_day, presence: true
-  validates :last_name, presence: true, length: {maximum: 20}
-  validates :first_name, presence: true, length: {maximum: 20}
-  validates :last_name_kana, presence: true, length: {maximum: 20}, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}
-  validates :first_name_kana, presence: true, length: {maximum: 20}, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}
+  validates :nickname, presence: true, length: {maximum: 20}, on: :validates_step1
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'は有効でありません。' }, on: :validates_step1
+  validates :password, presence: true, length: {minimum: 7, maximum: 30}, format: { with: VALID_PASSWORD_REGEX, message: 'は255文字以下に設定して下さい'}, on: :validates_step1
+  validates :birth_year, presence: true, on: :validates_step1
+  validates :birth_month, presence: true, on: :validates_step1
+  validates :birth_day, presence: true, on: :validates_step1
+  validates :last_name, presence: true, length: {maximum: 20}, on: :validates_step1
+  validates :first_name, presence: true, length: {maximum: 20}, on: :validates_step1
+  validates :last_name_kana, presence: true, length: {maximum: 20}, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step1
+  validates :first_name_kana, presence: true, length: {maximum: 20}, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step1
 
   # 電話番号入力情報
-  validates :phone_number, null: false, format: { with: VALID_PHONE_REGEX, message: 'は有効でありません。'}
+  validates :phone_number, null: false, format: { with: VALID_PHONE_REGEX, message: 'は有効でありません。'}, on: :validates_step2
 
   # アドレス情報
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}
-  validates :last_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}
-  validates :post_number, presence: true
-  validates :prefecture_id, presence: true
-  validates :city, presence: true
-  validates :address_number, presence: true
-  validates :building, presence: true
-  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX, message: 'は有効でありません。'}, allow_blank: true
+  validates :first_name, presence: true, on: :validates_step3
+  validates :last_name, presence: true, on: :validates_step3
+  validates :first_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step3
+  validates :last_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step3
+  validates :post_number, presence: true, on: :validates_step3
+  validates :prefecture_id, presence: true, on: :validates_step3
+  validates :city, presence: true, on: :validates_step3
+  validates :address_number, presence: true, on: :validates_step3
+  validates :building, presence: true, on: :validates_step3
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX, message: 'は有効でありません。'}, allow_blank: true, on: :validates_step3
   
 
-  
   protected
 
   def password_required?
