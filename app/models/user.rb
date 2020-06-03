8e21509e91ae6ea657c8398da43bc27d75b01670
kusa11 <59110220+kusa11@users.noreply.github.com>
2020-06-03 22:05:06 +0900

    正規表現の訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d!@#\$%\^\&*\)\(+=._-]{7,255}\z/i
+  VALID_kanji = /\A[一-龥\ァ-ン\ぁ-ん]+\z/
   VALID_KATAKANA_REGEX =  /\A[\p{katakana}\p{blank}ー－]+\z/
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
   VALID_POSTAL_CODE = /\A\d{3}-?\d{4}\z/
@@ -20,17 +21,19 @@ class User < ApplicationRecord
   validates :birth_year, presence: true, on: :validates_step1
   validates :birth_month, presence: true, on: :validates_step1
   validates :birth_day, presence: true, on: :validates_step1
-  validates :last_name, presence: true, length: {maximum: 20}, on: :validates_step1
-  validates :first_name, presence: true, length: {maximum: 20}, on: :validates_step1
+  validates :last_name, presence: true, length: {maximum: 20}, format: { with: VALID_kanji, message: 'は有効でありません。' }, on: :validates_step1
+  validates :first_name, presence: true, length: {maximum: 20}, format: { with: VALID_kanji, message: 'は有効でありません。' }, on: :validates_step1
   validates :last_name_kana, presence: true, length: {maximum: 20}, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step1
   validates :first_name_kana, presence: true, length: {maximum: 20}, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step1
 
   # 電話番号入力情報
   validates :phone_number, null: false, format: { with: VALID_PHONE_REGEX, message: 'は有効でありません。'}, on: :validates_step2
 
+  
+
   # アドレス情報
-  validates :first_name, presence: true, on: :validates_step3
-  validates :last_name, presence: true, on: :validates_step3
+  validates :first_name, presence: true, on: :validates_step3, format: { with: VALID_kanji, message: 'は有効でありません。' }, on: :validates_step3
+  validates :last_name, presence: true, on: :validates_step3, format: { with: VALID_kanji, message: 'は有効でありません。' }, on: :validates_step3
   validates :first_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step3
   validates :last_name_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}, on: :validates_step3
   validates :post_number, presence: true, on: :validates_step3
