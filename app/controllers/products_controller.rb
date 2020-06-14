16976b07a705caaedbd02d90b4e24d321457296d
gasashi <gasashi8264@gmail.com>
2020-06-15 00:04:36 +0900

    カテゴリー機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
   # 親カテゴリーが選択された後に動くアクション
   def get_category_children
     #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+    @category_children = Category.find(params[:category_id]).children
   end
 
   # 子カテゴリーが選択された後に動くアクション
